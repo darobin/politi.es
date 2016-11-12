4235e21e91fa3a3dba127833bc4c6921b8792f10
Robin Berjon <robin@berjon.com>
2016-11-12 14:50:20 -0500

    bring in the basics

---
+++ build.js
@@ -0,0 +1,9 @@
+
+// this is a really simple build script, if this takes off much better will be needed.
+
+let findit = require('findit')
+  , { join } = require('path')
+  , src = join(__dirname, 'src')
+  , res = join(__dirname, 'resources')
+  , out = join(__dirname, 'publish')
+;
