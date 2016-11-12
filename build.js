c3aca37c385ca0e8f89c8b4874ce0261346e2504
Robin Berjon <robin@berjon.com>
2016-11-12 15:12:51 -0500

    card pic

--- build.js
+++ build.js
@@ -3,7 +3,38 @@
 
 let findit = require('findit')
   , { join } = require('path')
+  , map = require('async/map')
   , src = join(__dirname, 'src')
   , res = join(__dirname, 'resources')
   , out = join(__dirname, 'publish')
 ;
+
+map(
+  [src, res, out],
+  treeView,
+  (err, [srcTree, resTree, outTree]) => {
+    if (err) throw err;
+    let deletes = Object.keys(outTree).filter(path => (!srcTree[path] && !resTree[path]));
+
+  }
+);
+
+
+function treeView (root, cb) {
+  let finder = findit(root)
+    , tree = {}
+  ;
+  finder.on('path', (fileName, stat) => {
+    let mtime = stat.mtime.getTime()
+      , ctime = stat.ctime.getTime()
+    ;
+    tree[fileName] = {
+      isDir:    stat.isDirectory(),
+      modified: (ctime > mtime) ? ctime : mtime,
+    };
+  });
+  finder.on('error', (err) => cb);
+  finder.on('end', () => {
+    cb(null, tree);
+  });
+}
