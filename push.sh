c2c5a26075277ef1935e829e7dd5dc1d01558998
Robin Berjon <robin@berjon.com>
2016-11-14 22:24:30 -0500

    new attempt

--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # git push origin `git subtree split --prefix publish master`:gh-pages
-git subtree push --prefix publish origin gh-pages
+# git subtree push --prefix publish origin gh-pages
 # try this one if the above doesn't work
-#   cd ./out && \
-#   remote_repo=`git config remote.origin.url` && \
-#   remote_branch="gh-pages" && \
-#   git init && \
-#   git add . && \
-#   git commit -m'build' && \
-#   git push $remote_repo master:$remote_branch --force && \
-#   rm -fr .git && \
-#   cd ../
+cd ./publish && \
+  remote_repo=`git config remote.origin.url` && \
+  remote_branch="gh-pages" && \
+  git init && \
+  git add . && \
+  git commit -m'build' && \
+  git push $remote_repo master:$remote_branch --force && \
+  rm -fr .git && \
+  cd ../
