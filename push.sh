95de4fad131df4ca9665dcfbec273125343ba1bb
Robin Berjon <robin@berjon.com>
2016-11-14 22:14:13 -0500

    deploy to gh-pages

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # git push origin `git subtree split --prefix publish master`:gh-pages
-git subtree push --prefix publish origin gh-pages
+# git subtree push --prefix publish origin gh-pages
+GIT_DEPLOY_DIR=publish ./deploy.sh
